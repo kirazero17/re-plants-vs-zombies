6d12f6aa257607972d6d41dc8366ac1a554a17b3
headshot2017 <nou@jm>
2024-08-09 21:47:35 -0400

    band-aid fix: draw lawnmower without using cache

--- Lawn/LawnMower.cpp
+++ Lawn/LawnMower.cpp
@@ -345,7 +345,8 @@ void LawnMower::Draw(Graphics* g)
         {
             aMowerType = LawnMowerType::LAWNMOWER_SUPER_MOWER;
         }
-        mApp->mReanimatorCache->DrawCachedMower(&aMowerGraphics, 0.0f, 19.0f, aMowerType);
+        //mApp->mReanimatorCache->DrawCachedMower(&aMowerGraphics, 0.0f, 19.0f, aMowerType);
+        mApp->ReanimationGet(mReanimID)->Draw(&aMowerGraphics);
     }
 }
 
