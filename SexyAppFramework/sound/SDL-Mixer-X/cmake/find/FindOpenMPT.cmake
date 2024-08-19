1298cdc94cf50e470d0f7d000070ca7965f8ed54
headshot2017 <nou@jm>
2024-08-19 18:35:03 -0400

    fix openmpt on linux

--- SexyAppFramework/sound/SDL-Mixer-X/cmake/find/FindOpenMPT.cmake
+++ SexyAppFramework/sound/SDL-Mixer-X/cmake/find/FindOpenMPT.cmake
@@ -1,31 +1,31 @@
 # - Try to find libopenmpt
 # Once done this will define
-#  OPENMPT_FOUND - System has libopenmpt
-#  OPENMPT_INCLUDE_DIRS - The libopenmpt include directories
-#  OPENMPT_LIBRARIES - The libraries needed to use libopenmpt
+#  OpenMPT_FOUND - System has libopenmpt
+#  OpenMPT_INCLUDE_DIRS - The libopenmpt include directories
+#  OpenMPT_LIBRARIES - The libraries needed to use libopenmpt
 
-find_path(OPENMPT_INCLUDE_DIR "libopenmpt.h"
+find_path(OpenMPT_INCLUDE_DIR "libopenmpt.h"
           PATH_SUFFIXES libopenmpt)
-find_library(OPENMPT_LIBRARY NAMES openmpt)
+find_library(OpenMPT_LIBRARY NAMES openmpt)
 
-if(OPENMPT_INCLUDE_DIR AND OPENMPT_LIBRARY)
+if(OpenMPT_INCLUDE_DIR AND OpenMPT_LIBRARY)
     if(APPLE)
-        find_library(OPENMPT_DYNAMIC_LIBRARY NAMES "openmpt"  PATH_SUFFIXES ".dylib")
+        find_library(OpenMPT_DYNAMIC_LIBRARY NAMES "openmpt"  PATH_SUFFIXES ".dylib")
     elseif(WIN32)
-        find_library(OPENMPT_DYNAMIC_LIBRARY NAMES "openmpt" PATH_SUFFIXES ".dll")
+        find_library(OpenMPT_DYNAMIC_LIBRARY NAMES "openmpt" PATH_SUFFIXES ".dll")
     else()
-        find_library(OPENMPT_DYNAMIC_LIBRARY NAMES "openmpt" PATH_SUFFIXES ".so")
+        find_library(OpenMPT_DYNAMIC_LIBRARY NAMES "openmpt" PATH_SUFFIXES ".so")
     endif()
 endif()
 
 include(FindPackageHandleStandardArgs)
-# handle the QUIETLY and REQUIRED arguments and set OPENMPT_FOUND to TRUE
+# handle the QUIETLY and REQUIRED arguments and set OpenMPT_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(OPENMPT  DEFAULT_MSG
-                                  OPENMPT_LIBRARY OPENMPT_INCLUDE_DIR)
+find_package_handle_standard_args(OpenMPT  DEFAULT_MSG
+                                  OpenMPT_LIBRARY OpenMPT_INCLUDE_DIR)
 
-mark_as_advanced(OPENMPT_INCLUDE_DIR OPENMPT_LIBRARY)
+mark_as_advanced(OpenMPT_INCLUDE_DIR OpenMPT_LIBRARY)
 
-set(OPENMPT_LIBRARIES ${OPENMPT_LIBRARY} )
-set(OPENMPT_INCLUDE_DIRS ${OPENMPT_INCLUDE_DIR} )
+set(OpenMPT_LIBRARIES ${OpenMPT_LIBRARY} )
+set(OpenMPT_INCLUDE_DIRS ${OpenMPT_INCLUDE_DIR} )
 
--- SexyAppFramework/sound/SDL-Mixer-X/src/codecs/music_openmpt.cmake
+++ SexyAppFramework/sound/SDL-Mixer-X/src/codecs/music_openmpt.cmake
@@ -4,35 +4,35 @@ if(USE_OPENMPT)
     option(USE_OPENMPT_STATIC "Use linking with a static OpenMPT" ON)
 
     if(USE_SYSTEM_AUDIO_LIBRARIES)
-        find_package(OPENMPT QUIET)
-        message("OpenMPT: [${OPENMPT_FOUND}] ${OPENMPT_INCLUDE_DIRS} ${OPENMPT_LIBRARIES}")
+        find_package(OpenMPT)
+        message("OpenMPT: [${OpenMPT_FOUND}] ${OpenMPT_INCLUDE_DIRS} ${OpenMPT_LIBRARIES}")
         if(USE_OPENMPT_DYNAMIC)
-            list(APPEND SDL_MIXER_DEFINITIONS -DOPENMPT_DYNAMIC=\"${OPENMPT_DYNAMIC_LIBRARY}\")
-            message("Dynamic OpenMPT: ${OPENMPT_DYNAMIC_LIBRARY}")
+            list(APPEND SDL_MIXER_DEFINITIONS -DOPENMPT_DYNAMIC=\"${OpenMPT_DYNAMIC_LIBRARY}\")
+            message("Dynamic OpenMPT: ${OpenMPT_DYNAMIC_LIBRARY}")
         endif()
 
     else()
         if(DOWNLOAD_AUDIO_CODECS_DEPENDENCY)
-            set(OPENMPT_LIBRARIES openmpt${MIX_DEBUG_SUFFIX})
+            set(OpenMPT_LIBRARIES openmpt${MIX_DEBUG_SUFFIX})
         else()
-            find_library(OPENMPT_LIBRARIES NAMES openmpt
+            find_library(OpenMPT_LIBRARIES NAMES openmpt
                          HINTS "${AUDIO_CODECS_INSTALL_PATH}/lib")
         endif()
-        set(OPENMPT_FOUND 1)
+        set(OpenMPT_FOUND 1)
         set(STDCPP_NEEDED 1) # Statically linking openmpt which is C++ library
-        set(OPENMPT_INCLUDE_DIRS "${AUDIO_CODECS_PATH}/libopenmpt/include")
+        set(OpenMPT_INCLUDE_DIRS "${AUDIO_CODECS_PATH}/libopenmpt/include")
     endif()
 
-    if(OPENMPT_FOUND)
+    if(OpenMPT_FOUND)
         message("== using libopenmpt ==")
         list(APPEND SDL_MIXER_DEFINITIONS -DMUSIC_MOD_OPENMPT -DOPENMPT_STATIC)
         if(USE_OPENMPT_STATIC)
             list(APPEND SDL_MIXER_DEFINITIONS)
         endif()
         if(NOT USE_SYSTEM_AUDIO_LIBRARIES OR NOT USE_OPENMPT_DYNAMIC)
-            list(APPEND SDLMixerX_LINK_LIBS ${OPENMPT_LIBRARIES})
+            list(APPEND SDLMixerX_LINK_LIBS ${OpenMPT_LIBRARIES})
         endif()
-        list(APPEND SDL_MIXER_INCLUDE_PATHS ${OPENMPT_INCLUDE_DIRS})
+        list(APPEND SDL_MIXER_INCLUDE_PATHS ${OpenMPT_INCLUDE_DIRS})
         list(APPEND SDLMixerX_SOURCES
             ${SDLMixerX_SOURCE_DIR}/src/codecs/music_openmpt.c
             ${SDLMixerX_SOURCE_DIR}/src/codecs/music_openmpt.h
