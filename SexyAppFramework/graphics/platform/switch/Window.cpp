a9ad6663414a563b6ee0d83b27800ed0f21958c1
headshot2017 <nou@jm>
2024-08-22 20:10:18 -0400

    use EGL on Switch MakeWindow()

--- SexyAppFramework/graphics/platform/switch/Window.cpp
+++ SexyAppFramework/graphics/platform/switch/Window.cpp
@@ -1,6 +1,8 @@
 #include <EGL/egl.h>    // EGL library
 #include <EGL/eglext.h> // EGL extensions
 
+#include <switch.h>
+
 #include "SexyAppBase.h"
 #include "graphics/GLInterface.h"
 #include "graphics/GLImage.h"
@@ -10,14 +12,59 @@ using namespace Sexy;
 
 void SexyAppBase::MakeWindow()
 {
-	if (mWindow)
+	// Connect to the EGL default display
+	mWindow = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+	if (!mWindow)
+		return;
+
+	// Initialize the EGL display connection
+	eglInitialize(mWindow, nullptr, nullptr);
+
+	// Select OpenGL ES as the desired graphics API
+	if (eglBindAPI(EGL_OPENGL_ES_API) == EGL_FALSE)
+		return;
+
+	// Get an appropriate EGL framebuffer configuration
+	EGLConfig config;
+	EGLint numConfigs;
+	static const EGLint framebufferAttributeList[] =
 	{
-		
-	}
-	else
+		EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
+		EGL_RED_SIZE,     8,
+		EGL_GREEN_SIZE,   8,
+		EGL_BLUE_SIZE,    8,
+		EGL_ALPHA_SIZE,   8,
+		EGL_DEPTH_SIZE,   24,
+		EGL_STENCIL_SIZE, 8,
+		EGL_NONE
+	};
+	eglChooseConfig(mWindow, framebufferAttributeList, &config, 1, &numConfigs);
+	if (numConfigs == 0)
 	{
-		
+		eglTerminate(mWindow);
+		return;
 	}
+
+	// Create an EGL window surface
+	mSurface = eglCreateWindowSurface(mWindow, config, nwindowGetDefault(), nullptr);
+	if (!mSurface)
+	{
+		eglTerminate(mWindow);
+		return;
+	}
+
+	// Create an EGL rendering context
+	static EGLint context_attribs[] = { EGL_CONTEXT_CLIENT_VERSION, 2, EGL_NONE };
+	mContext = eglCreateContext(mWindow, config, EGL_NO_CONTEXT, context_attribs);
+	if (!mContext)
+	{
+		eglDestroySurface(mWindow, mSurface);
+		eglTerminate(mWindow);
+		return;
+	}
+
+	// Connect the context to the surface
+	eglMakeCurrent(mWindow, mSurface, mSurface, mContext);
 
 	if (mGLInterface == NULL)
 	{
